599f98b862746e276b3cdde7994fd5fb7531739f
Goblom <goblom@me.com>
2013-11-11 16:54:13 -0800

    My IconMenu

--- src/main/java/org/goblom/bukkitlibs/IconMenu.java
+++ src/main/java/org/goblom/bukkitlibs/IconMenu.java
@@ -1,6 +1,8 @@
 package org.goblom.bukkitlibs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -9,6 +11,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,23 +27,35 @@ public class IconMenu implements Listener {
    
     private String[] optionNames;
     private ItemStack[] optionIcons;
-   
+    private String[] optionActions;
+    
+    private List<String> viewing = new ArrayList<String>();
+    
     public IconMenu(String name, int size, OptionClickEventHandler handler, Plugin plugin) {
         this.name = name;
         this.size = size;
         this.handler = handler;
         this.plugin = plugin;
         this.optionNames = new String[size];
+        this.optionActions = new String[size];
         this.optionIcons = new ItemStack[size];
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
-   
-    public IconMenu setOption(int position, ItemStack icon, String name, String... info) {
+    
+    public IconMenu setOption(int position, ItemStack icon, String name, String action, String... info) {
         optionNames[position] = name;
+        optionActions[position] = action;
         optionIcons[position] = setItemNameAndLore(icon, name, info);
         return this;
     }
-   
+    
+    public IconMenu setOption(int position, ItemStack icon, String name, String action, List<String> info) {
+        optionNames[position] = name;
+        optionActions[position] = action;
+        optionIcons[position] = setItemNameAndLore(icon, name, info);
+        return this;
+    }
+    
     public void open(Player player) {
         Inventory inventory = Bukkit.createInventory(player, size, name);
         for (int i = 0; i < optionIcons.length; i++) {
@@ -56,6 +72,7 @@ public class IconMenu implements Listener {
         plugin = null;
         optionNames = null;
         optionIcons = null;
+        optionActions = null;
     }
    
     @EventHandler(priority=EventPriority.MONITOR)
@@ -64,19 +81,30 @@ public class IconMenu implements Listener {
             event.setCancelled(true);
             int slot = event.getRawSlot();
             if (slot >= 0 && slot < size && optionNames[slot] != null) {
-                Plugin plugin = this.plugin;
-                OptionClickEvent e = new OptionClickEvent((Player)event.getWhoClicked(), slot, optionNames[slot]);
-                handler.onOptionClick(e);
-                if (e.willClose()) {
-                    final Player p = (Player)event.getWhoClicked();
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                        public void run() {
-                            p.closeInventory();
+                if (optionIcons[slot] != null) {
+                    if ((optionActions[slot] != null) && (!optionActions[slot].equals(""))) {
+                        Plugin plugin = this.plugin;
+                        OptionClickEvent e = new OptionClickEvent(
+                                (Player)event.getWhoClicked(), 
+                                slot, 
+                                optionNames[slot], 
+                                optionIcons[slot],
+                                optionActions[slot]
+                        );
+                        
+                        handler.onOptionClick(e);
+                        if (e.willClose()) {
+                            final Player p = (Player)event.getWhoClicked();
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                                public void run() {
+                                    p.closeInventory();
+                                }
+                            }, 1);
                         }
-                    }, 1);
-                }
-                if (e.willDestroy()) {
-                    destroy();
+                        if (e.willDestroy()) {
+                            destroy();
+                        }
+                    }
                 }
             }
         }
@@ -92,15 +120,17 @@ public class IconMenu implements Listener {
         private String name;
         private boolean close;
         private boolean destroy;
-       
-        public OptionClickEvent(Player player, int position, String name) {
+        private ItemStack item;
+        private String action;
+            
+        public OptionClickEvent(Player player, int position, String name, ItemStack item, String action) {
             this.player = player;
             this.position = position;
             this.name = name;
-            this.close = true;
-            this.destroy = false;
+            this.action = action;
+            this.item = item;
         }
-       
+        
         public Player getPlayer() {
             return player;
         }
@@ -111,6 +141,14 @@ public class IconMenu implements Listener {
        
         public String getName() {
             return name;
+        }
+        
+        public String getAction() {
+            return action;
+        }
+        
+        public ItemStack getMeta() {
+            return item;
         }
        
         public boolean willClose() {
@@ -137,5 +175,88 @@ public class IconMenu implements Listener {
         item.setItemMeta(im);
         return item;
     }
-   
+    
+    public ItemStack setItemNameAndLore(ItemStack item, String name, List<String> lore) {
+        ItemMeta im = item.getItemMeta();
+            im.setDisplayName(name);
+            im.setLore(lore);
+        item.setItemMeta(im);
+        return item;
+    }
+    
+    public IconMenu resetOptions() {
+        this.optionNames = new String[this.size];
+        this.optionActions = new String[this.size];
+        this.optionIcons = new ItemStack[this.size];
+        return this;
+    }
+
+    public void updateName(String name) { 
+        this.name = name; 
+    }
+    
+    public void updateSize(int size) { 
+        this.size = size; 
+    }
+    
+    public void updateAction(int position, String action) {
+        this.optionActions[position] = action;
+    }
+    
+    public void updateIcon(int position, String optionName, ItemStack icon) {
+        this.optionNames[position] = optionName;
+        this.optionIcons[position] = icon;
+    }
+    
+    public String getName() { 
+        return this.name; 
+    }
+    
+    public int getSize() { 
+        return this.size; 
+    }
+    
+    public String getAction(int position) {
+        return this.optionActions[position];
+    }
+    
+    public ItemStack getIcon(int position) {
+        return this.optionIcons[position];
+    }
+    
+    public String getIconName(int position) {
+        return this.optionNames[position];
+    }
+    
+    @EventHandler
+    public void onPluginDisable(PluginDisableEvent event) {
+        for (Player player : this.getViewers()) {
+            close(player);
+        }
+    }
+    
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (viewing.contains(event.getPlayer().getName())) viewing.remove(event.getPlayer().getName());
+    }
+    
+    public IconMenu close(Player player) {
+        if (player.getOpenInventory().getTitle().equals(this.name)) player.closeInventory();
+        return this;
+    }
+    
+    public List<Player> getViewers() {
+        List<Player> viewers = new ArrayList<Player>();
+        for (String s : viewing) {
+            viewers.add(Bukkit.getPlayer(s));
+        }
+        return viewers;
+    }
+    
+    public IconMenu closeViewers() {
+        for (Player player : this.getViewers()) {
+            close(player);
+        }
+        return this;
+    }
 }
