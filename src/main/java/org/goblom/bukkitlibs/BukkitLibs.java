2155c782290600bd2f650133609d945fce1ee3da
Goblom <goblom@me.com>
2013-11-18 13:51:05 -0800

    mongodb testing (not really much)

--- src/main/java/org/goblom/bukkitlibs/BukkitLibs.java
+++ src/main/java/org/goblom/bukkitlibs/BukkitLibs.java
@@ -29,4 +29,14 @@ package org.goblom.bukkitlibs;
  * 
  * @author Goblom
  */
-public class BukkitLibs { }
+public class BukkitLibs { 
+    public DatabaseManager.MongoDB mongoDB;
+    
+    public void onEnable() {
+        mongoDB = new DatabaseManager.MongoDB("localhost", "minecraft");
+    }
+    
+    public void MongoDBTest() {
+        
+    }
+}
