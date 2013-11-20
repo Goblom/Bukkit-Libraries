76d8f82af23921d22057913398f22169bfb54b7b
Goblom <goblom@me.com>
2013-11-20 12:07:14 -0800

    Small updates and fixes. Removed Plugin. Added ReflectCommand (hoping it works)

--- src/main/java/org/goblom/bukkitlibs/command/AbstractCommand.java
+++ src/main/java/org/goblom/bukkitlibs/command/AbstractCommand.java
@@ -24,15 +24,19 @@
 
 package org.goblom.bukkitlibs.command;
 
+import java.lang.reflect.Field;
 import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
-import org.bukkit.plugin.Plugin;
+
+import org.bukkit.craftbukkit.v1_6_R2.CraftServer;
 
 /**
  *
@@ -40,38 +44,36 @@ import org.bukkit.plugin.Plugin;
  */
 public abstract class AbstractCommand implements CommandExecutor {
     
-    protected final Plugin plugin;
-    
     protected final String command;
     protected final String description;
     protected final List<String> alias;
     protected final String usage;
     protected final String permMessage;
-    
+
+    protected static CommandMap cmap;
     protected PluginCommand plgCMD;
     
-    public AbstractCommand(Plugin plugin, String command) {
-        this(plugin, command, null, null, null, null);
+    public AbstractCommand(String command) {
+        this(command, null, null, null, null);
     }
     
-    public AbstractCommand(Plugin plugin, String command, String usage) {
-        this(plugin, command, usage, null, null, null);
+    public AbstractCommand(String command, String usage) {
+        this(command, usage, null, null, null);
     }
     
-    public AbstractCommand(Plugin plugin, String command, String usage, String description) {
-        this(plugin, command, usage, description, null, null);
+    public AbstractCommand(String command, String usage, String description) {
+        this(command, usage, description, null, null);
     }
     
-    public AbstractCommand(Plugin plugin, String command, String usage, String description, String permissionMessage) {
-        this(plugin, command, usage, description, permissionMessage, null);
+    public AbstractCommand(String command, String usage, String description, String permissionMessage) {
+        this(command, usage, description, permissionMessage, null);
     }
     
-    public AbstractCommand(Plugin plugin, String command, String usage, String description, List<String> aliases) {
-        this(plugin, command, usage, description, null, aliases);
+    public AbstractCommand(String command, String usage, String description, List<String> aliases) {
+        this(command, usage, description, null, aliases);
     }
     
-    public AbstractCommand(Plugin plugin, String command, String usage, String description, String permissionMessage, List<String> aliases) {
-        this.plugin = plugin;
+    public AbstractCommand(String command, String usage, String description, String permissionMessage, List<String> aliases) {
         this.command = command.toLowerCase();
         this.usage = usage;
         this.description = description;
@@ -82,11 +84,44 @@ public abstract class AbstractCommand implements CommandExecutor {
     }
     
     final void init() {
+        ReflectCommand cmd = new ReflectCommand(this.command);
+        getCommandMap().register("", cmd);
+        if (this.alias != null) cmd.setAliases(this.alias);
+        if (this.description != null) cmd.setDescription(this.description);
+        if (this.usage != null) cmd.setUsage(this.usage);
+        if (this.permMessage != null) cmd.setPermissionMessage(this.permMessage);
+        cmd.setExecutor(this);
+        
         plgCMD = Bukkit.getServer().getPluginCommand(this.command);
+        plgCMD.setExecutor(this);
         if (this.usage != null) plgCMD.setUsage(this.usage);
         if (this.description != null) plgCMD.setDescription(this.description);
         if (this.permMessage != null) plgCMD.setPermissionMessage(this.permMessage);
         if (this.alias != null) plgCMD.setAliases(this.alias);
+    }
+    
+    final CommandMap getCommandMap() {
+        if (cmap == null) {
+            try {
+                if (Bukkit.getServer() instanceof CraftServer) {
+                    final Field f = CraftServer.class.getDeclaredField("commandMap");
+                    f.setAccessible(true);
+                    cmap = (CommandMap) f.get(Bukkit.getServer());
+                    return getCommandMap();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        } else if (cmap != null) { return cmap; }
+        return getCommandMap();
+    }
+    
+    private final class ReflectCommand extends Command {
+        private CommandExecutor exe = null;
+        protected ReflectCommand(String command) { super(command); }
+        public void setExecutor(CommandExecutor exe) { this.exe = exe; }
+        public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+            if (exe != null) { exe.onCommand(sender, this, commandLabel, args); }
+            return false;
+        }
     }
     
     private boolean isPlayer(CommandSender sender) { return (sender instanceof Player); }
