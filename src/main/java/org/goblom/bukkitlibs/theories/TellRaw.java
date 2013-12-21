4340aa0273f21899d0a09471c77f2d6731e21510
Goblom <goblom@me.com>
2013-12-21 11:15:02 -0800

    JSON message for TellRaw like messages. Not Tested. In beta

---
+++ src/main/java/org/goblom/bukkitlibs/theories/TellRaw.java
@@ -0,0 +1,128 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2013 Goblom.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.goblom.bukkitlibs.theories;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author Goblom
+ */
+public class TellRaw {
+    private final List<String> messages = new ArrayList<String>();
+    
+    public TellRaw(String... message) {
+        for (String m : message) {
+            messages.add("text:'" + m + "',");
+        }
+    }
+    
+    public void addFormat(int messageIndex, Format format) {
+        String oldMessage = messages.get(messageIndex);
+        messages.set(messageIndex, oldMessage + format.getFormat() + ":" + true + ",");
+    }
+    
+    public void addColor(int messageIndex, ChatColor color) {
+        String oldMessage = messages.get(messageIndex);
+        messages.set(messageIndex, oldMessage + "color:" + color.name().toLowerCase() + ",");
+    }
+    
+    public void addClickEvent(int messageIndex, Action action, String value) {
+        String oldMessage = messages.get(messageIndex);
+        messages.set(messageIndex, oldMessage + "clickEvent:{action:" + action.getAction() + ",value:'" + value + "'}");
+    }
+    
+    public void addHoverEvent(int messageIndex, Action action, String value) {
+        String oldMessage = messages.get(messageIndex);
+        messages.set(messageIndex, oldMessage + "hoverEvent:{action:" + action.getAction() + ",value:'" + value + "'}");
+    }
+    
+    public String itemToJSON(ItemStack i) {
+        String json = "{id:" + i.getTypeId() + ",";
+        if (i.hasItemMeta()) {
+            json = json + "tag:{display:{";
+            if (i.getItemMeta().hasDisplayName()) {
+                json = json + "Name:" + i.getItemMeta().getDisplayName() + ",";
+            }
+            if (i.getItemMeta().hasLore()) {
+                String lores = "Lore:[";
+                for (String lore : i.getItemMeta().getLore()) {
+                    lores = "'" + lore + "'" + ",";
+                }
+                json = json + lores;
+            }
+            json = json + "]}}";
+        }
+        return json + "}";
+    }
+    
+    public enum Format {
+        BOLD("bold"), 
+        UNDERLINED("underlined"), 
+        ITALIC("italic"), 
+        STRIKETHROUGH("strikethrough"), 
+        OBFUSTICATED("obfusticated");
+        private final String format;
+        Format(String format) { this.format = format; }
+        public String getFormat() { return format; }
+    }
+    
+    public enum Action {
+        RUN_COMMAND("run_command"),
+        SHOW_TEXT("show_text"),
+        SHOW_ITEM("show_item"),
+        SHOW_ACHIEVEMENT("show_achievement"),
+        SUGGEST_COMMAND("suggest_command"),
+        OPEN_URL("open_url");
+        
+        private final String action;
+        Action(String action) { this.action = action; }
+        public String getAction() { return action; }
+    }
+    
+    private String removeExcess(String message) { //Fixes extra commas that have been added
+        return message.replaceAll(",]", "]").replaceAll(",}", "}");
+    }
+    
+    public List<String> getMessages() {
+        return messages;
+    }
+    
+    @Override
+    public String toString() {
+        String message = "{";
+        for (String m : messages) {
+            if (message.equals("{")) {
+                message = message + m + ",extra:["; 
+            } else {
+                message = "{" + message + m + "},";
+            }
+        }
+        return removeExcess(message + "}");
+    }
+}
