d97a35e7029e9e65101ee07d3e31d05b5597608a
Goblom <goblom@me.com>
2014-07-08 15:36:45 -0700

    move classes

--- src/main/java/org/goblom/bukkitlibs/utils/JarUtil.java
+++ src/main/java/org/goblom/bukkitlibs/utils/JarUtil.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.goblom.bukkitlibs;
+package org.goblom.bukkitlibs.utils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
