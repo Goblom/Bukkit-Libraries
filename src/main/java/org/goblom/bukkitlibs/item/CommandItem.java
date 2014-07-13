e8b02f124c4fc441ac140b96d1adac12fe69ad43
Goblom <goblom@me.com>
2014-07-13 15:06:58 -0700

    update CommandItem

--- src/main/java/org/goblom/bukkitlibs/item/CommandItem.java
+++ src/main/java/org/goblom/bukkitlibs/item/CommandItem.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2013 Goblom.
+ * Copyright 2014 Goblom.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,8 +21,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package org.goblom.bukkitlibs.item;
 
+import net.minecraft.util.org.apache.commons.lang3.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -33,198 +36,78 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 /**
- * Command Items. Creates any item with the ability to run a command from
- * anywhere in a world. This is a class file for my upcoming plugin
- * CommandItems.
  *
  * @author Goblom
  */
-public class CommandItem implements Listener {
-
-    private ItemStack item;
-    private String command;
-
+public class CommandItem {
     private Plugin plugin;
-    private UseEventHandler handler;
-
-    /**
-     * Create Command Item
-     *
-     * @param item ItemStack that the Command Item is attached to
-     * @param command Command that is attached to the ItemStack
-     * @param handler Handler to get data from the CommandItem
-     * @param plugin Your plugin
-     */
-    public CommandItem(ItemStack item, String command, UseEventHandler handler, Plugin plugin) {
-        this.item = item;
-        this.command = command;
-
-        this.plugin = plugin;
-        this.handler = handler;
+    private ItemStack item;
+    private UseRunner useRunner;
+    private Listener listener;
+    private Action[] runWith;
+    private boolean isDestroyed = false;
+    
+    public CommandItem(Plugin plugin, ItemStack itemStack, UseRunner runner, Action... actions) {
+        Validate.notNull(runWith, "You must have an action");
         
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
+        this.item = itemStack;
+        this.useRunner = runner;
+        this.runWith = actions;
+        
+        this.listener = new Listener() {
+            @EventHandler
+            public void onPlayerInteract(PlayerInteractEvent event) {
+                ItemStack handItem = event.getItem();
+                if (handItem == null) return;
+                for (Action action : runWith) {
+                    if (event.getAction().equals(action)) {
+                        if (handItem.isSimilar(item)) {
+                            useRunner.onUse(event.getPlayer());
+                            
+                            if (useRunner.willDestroy()) {
+                                destroy();
+                            }
+                            
+                            event.setCancelled(true);
+                        }
+                    }
+                }
+            }
+        };
+        
+        Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
     }
-
-    /**
-     * Set the command for the command item
-     *
-     * @param command command to set
-     * @return Command Item
-     */
-    public CommandItem setCommand(String command) {
-        this.command = command;
-        return this;
+    
+    public void destroy() {
+        HandlerList.unregisterAll(listener);
+        this.listener = null;
+        this.plugin = null;
+        this.item = null;
+        this.useRunner = null;
+        this.runWith = null;
+        this.isDestroyed = true;
     }
-
-    /**
-     * Sets the ItemStack for the command item.
-     *
-     * @param item itemstack to set
-     * @return this
-     */
-    public CommandItem setItem(ItemStack item) {
-        this.item = item;
-        return this;
+    
+    public boolean isDestroyed() {
+        return this.isDestroyed;
     }
-
-    /**
-     * gets the registered command
-     *
-     * @return command
-     */
-    public String getCommand() {
-        return command;
-    }
-
-    /**
-     * gets the item stack
-     *
-     * @return item
-     */
-    public ItemStack getItem() {
+    
+    public ItemStack getItemStack() {
         return item;
     }
-
-    /**
-     * Perform the command for the given Command Item
-     *
-     * @param player Player to perform the command
-     */
-    public void use(Player player) {
-        player.performCommand(command);
-    }
-
-    /**
-     * Give command item to a player.
-     *
-     * @param player Player to give the command item to.
-     */
-    public void giveItem(Player player) {
-        player.getInventory().addItem(item);
-    }
-
-    /**
-     * Destroy the Command Item (Mostly)
-     */
-    public void destroy() {
-        HandlerList.unregisterAll(this);
-        this.command = null;
-        this.item = null;
-        this.plugin = null;
-        this.handler = null;
-    }
-
-    public interface UseEventHandler {
-
-        public void onUseEvent(UseEvent event);
-    }
-
-    public class UseEvent {
-
-        private Player player;
-        private ItemStack item;
-
-        private String command;
-
+    
+    public static abstract class UseRunner {
         private boolean destroy;
-
-        /**
-         * @param player Player involved with this event
-         * @param item Itemstack involved with this event
-         * @param command Command from CommandItem
-         */
-        public UseEvent(Player player, ItemStack item, String command) {
-            this.player = player;
-            this.item = item;
-            this.command = command;
-        }
-
-        /**
-         * Player involved in this event
-         * @return Player involved in event
-         */
-        public Player getPlayer() {
-            return player;
-        }
-
-        /**
-         * Gets command associated with the item
-         * @return Command to run
-         */
-        public String getCommand() {
-            return command;
-        }
-
-        /**
-         * Gets the itemstack from this event
-         * @return ItemStack
-         */
-        public ItemStack getItem() {
-            return item;
-        }
-
-        /**
-         * Should we make the item act like a normal item now.
-         * @param destroy 
-         */
-        public void setWillDstroy(boolean destroy) {
-            this.destroy = destroy;
-        }
-
-        /**
-         * Should we destroy the item?
-         * @return destroy
-         */
-        public boolean willDestroy() {
-            return destroy;
+        
+        public final void setWillDestroy(boolean bln) {
+            this.destroy = bln;
         }
         
-        /**
-         * Run the command associated with this event
-         * @return true if successful, false if not
-         */
-        public boolean run() {
-            return player.performCommand(command);
+        public final boolean willDestroy() {
+            return this.destroy;
         }
-    }
-
-    @EventHandler
-    void onPlayerInteract(PlayerInteractEvent event) {
-        if (!event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            return;
-        }
-        if (event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            ItemStack handItem = event.getItem();
-            if (handItem.isSimilar(item)) {
-                UseEvent ue = new UseEvent(
-                        event.getPlayer(),
-                        event.getItem(),
-                        command
-                );
-                handler.onUseEvent(ue);
-                if (ue.willDestroy()) destroy();
-                event.setCancelled(true);
-            }
-        }
+        
+        public abstract void onUse(Player player);
     }
 }
