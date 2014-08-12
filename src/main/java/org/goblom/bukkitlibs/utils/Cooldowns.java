29924e73ad76939b48ae7df0def15c2cb357ab29
Goblom <goblom@me.com>
2014-08-12 03:51:28 -0700

    Cooldowns Pro... with events!

--- src/main/java/org/goblom/bukkitlibs/utils/Cooldowns.java
+++ src/main/java/org/goblom/bukkitlibs/utils/Cooldowns.java
@@ -43,41 +43,45 @@ import org.bukkit.scheduler.BukkitRunnable;
  * @author Goblom
  */
 public class Cooldowns {
-    
+
     private static final JavaPlugin PLUGIN = JavaPlugin.getProvidingPlugin(Cooldowns.class);
     private static final Map<UUID, Map<String, Cooldown>> cooldown_holder = Maps.newHashMap();
-    
+
     public static Cooldown prepare(Player player, String cooldownName, int seconds) {
         Map<String, Cooldown> player_cooldowns = (cooldown_holder.get(player.getUniqueId()) == null ? Maps.<String, Cooldown>newHashMap() : cooldown_holder.get(player.getUniqueId()));
-        if (player_cooldowns.containsKey(cooldownName)) {
-            return player_cooldowns.get(cooldownName);
-        }
-        
+
         Cooldown cooldown = new Cooldown();
                  cooldown.secondsLeft = seconds;
                  cooldown.uuid = player.getUniqueId();
                  cooldown.name = cooldownName;
                  cooldown.started = false;
-                 
+
         player_cooldowns.put(cooldownName, cooldown);
         cooldown_holder.put(player.getUniqueId(), player_cooldowns);
-        
+
         return cooldown;
     }
-    
+
+    public static void removeCooldown(Player player, String cooldownName) {
+        Map<String, Cooldown> player_cooldowns = (cooldown_holder.get(player.getUniqueId()) == null ? Maps.<String, Cooldown>newHashMap() : cooldown_holder.get(player.getUniqueId()));
+
+        player_cooldowns.remove(cooldownName);
+        cooldown_holder.put(player.getUniqueId(), player_cooldowns);
+    }
+
     public static Cooldown getCooldown(Player player, String cooldownName) {
         Map<String, Cooldown> player_cooldowns = (cooldown_holder.get(player.getUniqueId()) == null ? Maps.<String, Cooldown>newHashMap() : cooldown_holder.get(player.getUniqueId()));
-        
+
         return player_cooldowns.get(cooldownName);
     }
-    
+
     public static List<Cooldown> getCooldowns(Player player) {
         List<Cooldown> list = Lists.newArrayList();
         Map<String, Cooldown> player_cooldowns = (cooldown_holder.get(player.getUniqueId()) == null ? Maps.<String, Cooldown>newHashMap() : cooldown_holder.get(player.getUniqueId()));
         list.addAll(player_cooldowns.values());
         return Collections.unmodifiableList(list);
     }
-    
+
     public static List<Cooldown> getCooldownsWithSecondsLeft(Player player) {
         List<Cooldown> list = Lists.newArrayList();
         for (Cooldown cd : getCooldowns(player)) {
@@ -85,52 +89,62 @@ public class Cooldowns {
                 list.add(cd);
             }
         }
-        
+
         return Collections.unmodifiableList(list);
     }
-    
+
     public static class Cooldown {
+
         private int secondsLeft;
         private UUID uuid;
         private boolean started;
         private String name;
         private CooldownRunner runner;
-        
-        private Cooldown() {}
-        
+
+        private Cooldown() {
+        }
+
         public String getName() {
             return this.name;
         }
-        
+
         public Player getPlayer() {
             return Bukkit.getPlayer(uuid);
         }
-        
+
         public int getSecondsLeft() {
             return this.secondsLeft;
         }
-        
+
         public boolean hasTimeLeft() {
             return getSecondsLeft() != 0;
         }
-        
+
         public boolean hasStarted() {
             return this.started;
         }
-        
+
         public void setSecondsLeft(int secondsLeft) {
             this.secondsLeft = secondsLeft;
+        }
+
+        public void delete() {
+            Cooldowns.removeCooldown(getPlayer(), getName());
         }
         
         public Cooldown start() {
             if (!hasStarted() && this.runner == null) {
-                this.runner = new CooldownRunner(this);
-                this.runner.runTaskTimerAsynchronously(PLUGIN, -1, 20);
+                CooldownStartEvent event = new CooldownStartEvent(this);
+                Bukkit.getPluginManager().callEvent(event);
+                if (!event.isCancelled()) {
+                    this.runner = new CooldownRunner(this);
+                    this.runner.runTaskTimerAsynchronously(PLUGIN, -1, 20);
+                }
             }
-            
+
             return this;
         }
-        
+
         public void forceStop() {
             if (hasStarted() && this.runner != null) {
                 this.runner.cancel();
@@ -138,33 +152,36 @@ public class Cooldowns {
             }
         }
     }
-    
+
     private static class CooldownRunner extends BukkitRunnable {
 
         private final Cooldown cooldown;
-        
+
         CooldownRunner(Cooldown cooldown) {
             this.cooldown = cooldown;
         }
-        
+
         public void run() {
             if (cooldown.secondsLeft == 0) {
+                Bukkit.getPluginManager().callEvent(new CooldownFinishEvent(cooldown));
                 cancel();
                 return;
             }
-            
+
             cooldown.secondsLeft--;
         }
     }
-    
+
     public static class CooldownStartEvent extends Event implements Cancellable {
+
         private static final HandlerList handlers = new HandlerList();
         private final Cooldown cooldown;
         private boolean cancelled = false;
+
         CooldownStartEvent(Cooldown cooldown) {
             this.cooldown = cooldown;
         }
-        
+
         public Cooldown getCooldown() {
             return this.cooldown;
         }
@@ -173,7 +190,7 @@ public class Cooldowns {
         public HandlerList getHandlers() {
             return handlers;
         }
-        
+
         public static HandlerList getHandlerList() {
             return handlers;
         }
@@ -186,11 +203,27 @@ public class Cooldowns {
             this.cancelled = cancel;
         }
     }
-    
+
     public static class CooldownFinishEvent extends Event {
+
         private static final HandlerList handlers = new HandlerList();
         private final Cooldown cooldown;
+
+        CooldownFinishEvent(Cooldown cooldown) {
+            this.cooldown = cooldown;
+        }
         
-        public 
+        public Cooldown getCooldown() {
+            return this.cooldown;
+        }
+        
+        @Override
+        public HandlerList getHandlers() {
+            return handlers;
+        }
+
+        public static HandlerList getHandlerList() {
+            return handlers;
+        }
     }
 }
