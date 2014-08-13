6194aef8983ec9cb7bf1871cc12e8bc545b8738a
Goblom <goblom@me.com>
2014-08-12 18:15:07 -0700

    cleanup of crap code.

--- src/main/java/org/goblom/bukkitlibs/utils/ReflectionUtil.java
+++ src/main/java/org/goblom/bukkitlibs/utils/ReflectionUtil.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.goblom.bukkitlibs;
+package org.goblom.bukkitlibs.utils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
